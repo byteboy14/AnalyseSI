05a6d9cbaf1c14eb776a43b5fa392894d0309c5f
Tedi <kakatsi.tedi@gmail.com>
2025-06-29 01:41:08 +0000

    UPDATE : App state function

--- src/org/analyse/core/state/AppState.java
+++ src/org/analyse/core/state/AppState.java
@@ -2,9 +2,16 @@ package org.analyse.core.state;
 
 public class AppState {
     private String fileName ;
+    private String title ;
 
     public AppState() {
         this.fileName = "";
+        this.title = "";
+    }
+
+    public AppState(AppState state ) {
+        this.fileName = state.getFileName();
+        this.title = state.getTitle();
     }
 
     public String getFileName() {
@@ -13,5 +20,25 @@ public class AppState {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isEquals(AppState state ){
+
+        try {
+            if (!fileName.equals(state.getFileName())) return false;
+            if (!title.equals(state.getTitle())) return false;
+        }catch (Exception e) {
+            return false;
+        }
+        
+        return true ;
     }
 }
---
+++ test/org/analyse/core/state/AppStateTest.java
@@ -0,0 +1,63 @@
+package org.analyse.core.state;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class AppStateTest {
+
+    private AppState state ;
+    @Before
+    public void setup(){
+        state = new AppState();
+    }
+
+    @Parameterized.Parameters(name = "{index}: expected = {4}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { "","","", "", true } ,
+                {"","","","fileName", false},
+                { "","","title", "", false },
+                { "title","","title", "", true } ,
+                { "title","file name","title", "file", false } ,
+                { "title","file name","title", "file name", true } ,
+        });
+    }
+
+    private final String fileName ;
+    private final String title;
+
+    private final String fileName2 ;
+    private final String title2;
+
+    private final boolean expected;
+    public AppStateTest(String fileName , String title ,
+                        String fileName2 , String title2,
+                        boolean expected){
+        this.fileName = fileName;
+        this.title = title;
+        this.fileName2 = fileName2;
+        this.title2 = title2;
+        this.expected = expected;
+    }
+
+    @Test
+    public void isEquals() {
+        state.setTitle(title);
+        state.setFileName(fileName);
+        AppState newState = new AppState();
+        newState.setTitle(title2);
+        newState.setFileName(fileName2);
+
+
+        assertEquals(state.isEquals(newState), expected);
+    }
+}
